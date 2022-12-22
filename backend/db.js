4f346e56678cf01b5417fd1dd69ec20a6c54d0ef
Ankit <ankit93100@gmail.com>
2022-12-22 15:14:48 +0530

    Added comments in index and db

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,10 @@
+/*
+ Include mongoose - Object Data Modelling (ODM) library for MongoDB
+ Provides an interface to the database for creating, quering, updating, deleting records, etc. 
+*/
 const mongoose = require("mongoose");
 
+// Setup connection with database
 const connectToMongo = () => {
   mongoose.connect("mongodb://localhost:27017/contractsData", () => {
     console.log("Connected to MongoDB Successfully!!!");
